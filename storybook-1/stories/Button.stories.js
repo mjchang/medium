a88fd773542249b5561d11f5182f34dcfce1c49e
chang <michael@xage.com>
2020-10-19 16:30:18 -0700

    Add Storybook

---
+++ storybook-1/stories/Button.stories.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: {
+    label: 'Button',
+  },
+  parameters: {
+    controls: {
+      expanded: true,
+    },
+  },
+  decorators: [
+    Story => (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+const Template = args => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
