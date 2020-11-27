3b9f9dc95c56a13f3fb13610df7879d3d28f3092
chang <michael@xage.com>
2020-11-27 15:53:16 -0800

    Add integer input

---
+++ integer-input/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import IntegerInput from './IntegerInput';
+
+const App = () => {
+  const [value, setValue] = useState('');
+  return (
+    <>
+      <h1>Integer Input</h1>
+      <IntegerInput onChange={setValue} value={value} />
+    </>
+  );
+};
+
+export default App;
---
+++ integer-input/src/IntegerInput.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const IntegerInput = props => {
+  const { onChange, value } = props;
+  return (
+    <input
+      onChange={e => onChange(e.target.value)}
+      onKeyPress={e => {
+        if (!/[-\d]/.test(e.key)) {
+          e.preventDefault();
+        }
+      }}
+      type="number"
+      value={value}
+    />
+  );
+};
+IntegerInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+};
+IntegerInput.defaultProps = {
+  value: '',
+};
+
+export default IntegerInput;
